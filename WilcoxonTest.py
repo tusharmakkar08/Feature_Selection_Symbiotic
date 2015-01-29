9836f0b9ba4487a979a246104c14732bc6ae0a86
Tushar Makkar <tusharmakkar08@gmail.com>
2015-01-29 11:36:54 +0530

    Wilcoxon test made and tested

--- WilcoxonTest.py
+++ WilcoxonTest.py
@@ -17,5 +17,21 @@ def WilcoxonTest(Original_input, Symbiotic_output, Modified_GA_output):
         Modified_GA_output: Data with N-1 features extracted using 
                             Modified Genetic algorithm
     Returns:
-        The best suited N-1 features for a given distribution
+        The best suited N-1 features for a given distribution or [-1]
     '''
+    z_stat_for_symbiotic, p_val_for_symbiotic = stats.ranksums(
+                            Symbiotic_output, Original_input)
+    z_stat_for_GA, p_val_for_GA = stats.ranksums(
+                            Modified_GA_output, Original_input)
+    print p_val_for_GA , p_val_for_symbiotic
+    if (p_val_for_GA < 0.5 and p_val_for_symbiotic < 0.5) : 
+        return [-1]
+    elif (p_val_for_GA > p_val_for_symbiotic): 
+        return Modified_GA_output
+    else: 
+        return Symbiotic_output
+        
+#~ 
+#~ if __name__ == '__main__':
+    #~ print WilcoxonTest([1,2,3,4], [1,2,3,5],[0,9,10,11])
+#~ 
