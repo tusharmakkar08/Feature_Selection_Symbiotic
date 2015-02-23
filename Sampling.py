a3693f48bf5cd3a33d262cf08e5f85b8068e4dab
Tushar Makkar <tusharmakkar08@gmail.com>
2015-02-23 20:24:47 +0530

    2 to 3 sampling made

--- Sampling.py
+++ Sampling.py
@@ -6,7 +6,7 @@ Date: 5.02.2015
 
 import datareader, mushrooms_bfgs, mlpy, nltk, PGCM
 
-def GreedySampling (PGCM_matrix, OldPGCM, numberOfFeatures, Aggregation):
+def GreedySampling (PGCM_matrix, oldPGCM, numberOfFeatures, Aggregation):
     '''
     Returns the Sampled Matrix with the Aggregation Function used
     Args:
@@ -20,14 +20,21 @@ def GreedySampling (PGCM_matrix, OldPGCM, numberOfFeatures, Aggregation):
     X = {}
     Y = {}
     score = {}
-    for i in xrange(1, N+1): 
-        x[i] = PGCM_0[(i,i)]
+    for i in xrange(1, numberOfFeatures+1): 
+        X[i] = oldPGCM[(i,i)]
         score[i] = 100
-            for j in xrange(1,N+1):
-                if i != j:
-                    Y[(i,j)] = PGCM_0[(i,j)]
-                    score[i] = min(score[i], PGCM_0[(i,j)])
-    print score[i]
+        for j in xrange(i+1, numberOfFeatures+1):
+            Y[(i,j)] = PGCM_matrix[(i,j)]
+            if Aggregation == "min":
+                score[i] = min(score[i], PGCM_matrix[(i,j)])
+            if Aggregation == "max":
+                score[i] = max(score[i], PGCM_matrix[(i,j)])
+            if Aggregation == "avg":
+                score[i] = score[i]+PGCM_matrix[(i,j)]
+                print score[i]
+        if Aggregation == "avg":
+            score[i] = score[i]/(numberOfFeatures-i+1)
+    print score
     
 def test_initialize():
     (train_X, train_y, test_X, test_y) = mushrooms_bfgs.initialize(float(99)/100)
@@ -35,8 +42,8 @@ def test_initialize():
     print "Number of Testing Data =",len(test_y)
     PGCM_0 = PGCM.makePairs(train_X, train_y, test_X, test_y)
     oldPGCM = PGCM_0
-    N = 21 
-    GreedySampling(PGCM_0, OldPGCM, N,"min")
+    N = 22
+    GreedySampling(PGCM_0, oldPGCM, 22, "avg")
     
 if __name__ == "__main__":
     test_initialize()
