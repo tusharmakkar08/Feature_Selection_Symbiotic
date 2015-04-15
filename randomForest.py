da8b14fc6635a3db409bc114e7cdbcf4a5888c40
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-15 16:32:54 +0530

    randomForest classifier strengthened.

--- randomForest.py
+++ randomForest.py
@@ -21,9 +21,29 @@ def featureReduction(X, Y):
     for i in featureImportanceList : 
         featureImportanceDictionary[featureCount] = i 
         featureCount += 1
+    return featureImportanceDictionary
     sortedFeatureDictionary = sorted(featureImportanceDictionary.items(), key=operator.itemgetter(1))
-    print sortedFeatureDictionary
+    return sortedFeatureDictionary[1:]
+    #~ print len(sortedFeatureDictionary[1:])
 
+def netFeatureImportance(X, Y, N):
+    '''
+    Gives feature importances of features using tree classifier
+    INPUT
+    X : Features
+    Y : Output
+    N : Number of times we run random forest classifier
+    '''
+    featureImportance = featureReduction(X, Y)
+    for i in xrange(N - 1): 
+        trialImportance = featureReduction(X, Y)
+        for j in trialImportance:
+            featureImportance[j] += trialImportance[j]
+    for i in featureImportance:
+        featureImportance[i] /= N
+    sortedFeatureDictionary = sorted(featureImportance.items(), key=operator.itemgetter(1))
+    return sortedFeatureDictionary[1:]
+    
 if __name__ == '__main__':
-    (train_X, train_y, test_X, test_y) = mushrooms_bfgs.initialize(float(99)/100)
-    featureReduction(train_X, train_y)
+    (train_X, train_y, test_X, test_y) = mushrooms_bfgs.initialize(float(1)/100)
+    print netFeatureImportance(train_X, train_y, 15)
