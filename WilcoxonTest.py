a088cab763afea3cbe593d53296c5af36f5505e3
Tushar Makkar <tusharmakkar08@gmail.com>
2015-04-15 19:39:29 +0530

    Wilcoxon output adjusted

--- WilcoxonTest.py
+++ WilcoxonTest.py
@@ -26,15 +26,21 @@ def WilcoxonTest(Original_input, Symbiotic_output, Modified_GA_output):
                             Modified_GA_output, Original_input)
     print p_val_for_GA , p_val_for_symbiotic
     if (p_val_for_GA > p_val_for_symbiotic): 
-        return "Forest one is better"
+        print "Forest one is better"
+        return 1
     else: 
-        return "Symbiotic is better"
+        print "Symbiotic is better"
+        return 0
         
-
 if __name__ == '__main__':
     (train_X, train_y, test_X, test_y) = mushrooms_bfgs.initialize(float(80)/100)
     (train_X_Sym, test_X_Sym) = mushrooms_bfgs.featureRemoval(train_X, test_X, [3])
     (train_X_For, test_X_For) = mushrooms_bfgs.featureRemoval(train_X, test_X, [16])
     #~ print train_X[0], train_X_Sym[0], train_X_For[0]
-    print WilcoxonTest(train_X.ravel(), train_X_Sym.ravel() ,train_X_For.ravel())
+    if WilcoxonTest(train_X.ravel(), train_X_Sym.ravel() ,train_X_For.ravel()):
+        newData_Tr = train_X_For
+        newData_Ts = test_X_For
+    else:
+        newData_Tr = train_X_Sym
+        newData_Ts = test_X_Sym
     #~ print WilcoxonTest([1,2,3,4], [1,2,3,5],[0,9,10,11])
